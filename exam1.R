7aa0b2cf3bbb685e45868ce01395ea2e21353c91
emeliefoster <emf6966@louisiana.edu>
2021-02-24 15:48:29 -0600

    fixed zoop file

--- exam1.R
+++ exam1.R
@@ -7,7 +7,29 @@ year=str_sub(string=e$cruise, start=1,end=4)
 e$year=year
 View(e)
 colnames(e)
-eggs_corrected= e[, -c(1:5,8:20,23:28)]
+eggs_corrected1= e[, -c(2:5,8:20,23:28)]
 View(eggs_corrected)
 
-write.csv(x=eggs_corrected,file="C:/Users/Em/Documents/Course Materials/Spring 2021/Data Management/Data/exam 1/exam1_data/eggs_CUFES_fixed.csv",row.names=F)
+write.csv(x=eggs_corrected1,file="C:/Users/Em/Documents/Course Materials/Spring 2021/Data Management/Data/exam 1/exam1_data/eggs_CUFES_fixed1.csv",row.names=F)
+#processing zoop
+#fixing latitude
+z=read.csv("195101-201404_Zoop.csv")
+colnames(z)
+str(z)
+lat.c=z$Lat_Deg+z$Lat_Min/60
+head(lat.c)
+#fixing longitude 
+lon.c=(z$Lon_Deg+z$Lon_Min/60)*-1
+head(lon.c)
+z$LAT=lat.c
+z$LONG=lon.c
+View(z)
+year=str_sub(string=z$Tow_Date, start=6,end=9)
+head(year)
+year=as.integer(z$year)
+z$year=year
+str(z)
+colnames(z)
+zoop_corrected= z[, -c(1,3:9,11:19,24:26)]
+colnames(zoop_corrected)
+write.csv(x=zoop_corrected,file="C:/Users/Em/Documents/Course Materials/Spring 2021/Data Management/Data/exam 1/exam1_data/Zoop_fixed.csv",row.names=F)
